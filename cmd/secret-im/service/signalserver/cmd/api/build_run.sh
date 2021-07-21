e57fe632113f0ebd3b8134a8db17228436f4ecbc
dabaicai <dabaicai@gmail.com>
2021-07-21 10:01:30 +0800

    pro

--- cmd/secret-im/service/signalserver/cmd/api/build_run.sh
+++ cmd/secret-im/service/signalserver/cmd/api/build_run.sh
@@ -1,3 +1,4 @@
 go  build   -o ./deploy/signalserver.exe ./signalserver.go
 echo "zh" |sudo -S chmod +x ./deploy/signalserver.exe
-./deploy/signalserver.exe -f ./etc/signalserver-api.yaml
+cd ./deploy
+./signalserver.exe -f ../etc/signalserver-api.yaml
